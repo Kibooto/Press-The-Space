93fd63492131ece4c248bfc856adb92bc10e8049
Kibooto <oleksijc1@gmail.com>
2023-07-24 14:07:42 +0300

    added physics for coins

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import time
 import random
+import math
 
 WIDTH = 400
 HEIGHT = 600
@@ -36,7 +37,9 @@ press_times = []
 money = []
 moneyX = []
 moneyY = []
-money_number = 0
+moneyV = []
+moneyA = []
+moneyG = []
 
 moneyImg = pygame.image.load("assets/images/coin/coin.png")
 moneyImg = pygame.transform.scale(moneyImg, (25, 25)).convert()
@@ -45,37 +48,37 @@ moneyImg = pygame.transform.scale(moneyImg, (25, 25)).convert()
 shop_rect = pygame.Rect(100, 150, 200, 300)
 shop_bg = (220, 220, 220)
 
-def mf_space_create(moneyImg, money, moneyX, moneyY, money_number):
+def mf_space_create(moneyImg, money, moneyX, moneyY, moneyV, moneyA, moneyG):
     money.append(moneyImg)
     moneyX.append(random.randint(100, 275))
     moneyY.append(475)
-    money_number += 1
-    for i in range(money_number):
-        surface.blit(money[i], (moneyX[i],  moneyY[i]))
+    moneyV.append(random.uniform(10, 15))  # Випадкова швидкість
+    moneyA.append(random.uniform(60, 120))  # Випадковий кут у градусах
+    moneyG.append(0.5)
 
-    for i in range(len(money)-1, -1, -1):
-        if moneyY[i] <= 0:
-            money.pop(i)
-            moneyY.pop(i)
-            moneyX.pop(i)
-            money_number -= 1
+def mf_space_update(moneyX, moneyY, moneyV, moneyA, moneyG):
+    dt = 1 / 60  # Крок часу (60 кадрів в секунду)
+    for i in range(len(moneyX)):
+        # Оновлення координат монети
+        moneyX[i] += moneyV[i] * math.cos(math.radians(moneyA[i])) * dt
+        moneyY[i] -= moneyV[i] * math.sin(math.radians(moneyA[i])) * dt - 0.5 * moneyG[i] * dt**2
+    if len(money) >= 1:
+        for i in range(len(money)-1, -1, -1):
+            if moneyY[i] <= -20:
+                money.pop(i)
+                moneyX.pop(i)
+                moneyY.pop(i)
+                moneyV.pop(i)
+                moneyA.pop(i)
+                moneyG.pop(i)
     
     print(len(money))
-    return money_number
-    
-def mf_space_update(money_number, moneyX, moneyY): ##
-    for i in range(money_number):
-        moneyY[i] -= 1/3
-        moneyX[i] = moneyX[i] + random.uniform(-1,1)
-        surface.blit(money[i], (moneyX[i], moneyY[i]))
 
-    return 
 
 def time_draw():
     clock_font = pygame.font.Font(text_font, 22)
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
-
     return clock_font.render(f'Time: {current_time}', True, (0, 0, 0))
 
 def sp_draw(space):
@@ -92,7 +95,6 @@ def update_press_times():
 def count_presses_in_last_6_seconds():
     current_time = pygame.time.get_ticks() // 1000
     update_press_times()
-
     return round(len(press_times) / 6)
 
 running = True
@@ -100,22 +102,22 @@ running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print("GAME: Exited by click")
             pygame.quit()
             running = False
+            print("GAME: Exited by click")
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                print("GAME: Exited by escape")
                 pygame.quit()
                 running = False
+                print("GAME: Exited by escape")
 
             if event.key == pygame.K_SPACE:
                 space = True
                 if state == "game":
                     score += 1
                     press_times.append(pygame.time.get_ticks() // 1000)
-                    money_number = mf_space_create(moneyImg, money, moneyX, moneyY, money_number)
+                    mf_space_create(moneyImg, money, moneyX, moneyY, moneyV, moneyA, moneyG)
 
             if event.key == pygame.K_LALT:
                 if state == "game":
@@ -132,23 +134,30 @@ while running:
                 space = False
 
     if state == "game":
-            surface.fill(BG_COLOR)
-            sp_draw(space)
-            score_text = font.render(f'Score: {score}', True, (0, 0, 0))
-            pps_text = font.render(f'PPS: {count_presses_in_last_6_seconds()}', True, (0, 0, 0))
-            clock_text = time_draw()
-            mf_space_update(money_number, moneyX, moneyY)
-            surface.blit(score_text, (10, 10))
-            surface.blit(clock_text, (290, 10))
-            surface.blit(pps_text, (10, 30))
+        surface.fill(BG_COLOR)
+        sp_draw(space)
+        score_text = font.render(f'Score: {score}', True, (0, 0, 0))
+        pps_text = font.render(f'PPS: {count_presses_in_last_6_seconds()}', True, (0, 0, 0))
+        clock_text = time_draw()
 
-            pygame.display.flip()
-        
+        # Оновлення монет
+        mf_space_update(moneyX, moneyY, moneyV, moneyA, moneyG)
+
+        # Виведення монет на екран
+        for i in range(len(money)):
+            surface.blit(money[i], (moneyX[i], moneyY[i]))
+
+        surface.blit(score_text, (10, 10))
+        surface.blit(clock_text, (290, 10))
+        surface.blit(pps_text, (10, 30))
     elif state == "shop":
-        surface.fill((255,255,255), (270, 0, 130, 30))
+        surface.fill((255, 255, 255), (270, 0, 130, 30))
         pygame.draw.rect(surface, shop_bg, shop_rect)
         shop_text = font.render("Shop", True, (0, 0, 0))
         surface.blit(shop_text, (100, 150))
         clock_text = time_draw()
         surface.blit(clock_text, (290, 10))
-        pygame.display.flip()
+
+    pygame.display.flip()
+
+
